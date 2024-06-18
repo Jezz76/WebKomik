e9889cd3981343badbc8de75d6e7a40b957b0dcb
Jezz76 <144241540+Jezz76@users.noreply.github.com>
2024-06-18 20:03:35 +0700

    Update TerangGelap.js

--- TerangGelap.js
+++ TerangGelap.js
@@ -1,14 +1,28 @@
-document.getElementById('mode-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('light-mode');
-    const modeIcon = this.querySelector('i');
-    if (document.body.classList.contains('light-mode')) {
+document.addEventListener('DOMContentLoaded', function() {
+    // Periksa status mode saat halaman dimuat
+    if (localStorage.getItem('theme') === 'light') {
+        document.body.classList.add('light-mode');
+        const modeIcon = document.getElementById('mode-toggle').querySelector('i');
         modeIcon.classList.remove('fa-moon');
         modeIcon.classList.add('fa-sun');
-    } else {
-        modeIcon.classList.remove('fa-sun');
-        modeIcon.classList.add('fa-moon');
     }
+
+    // Tambahkan event listener untuk tombol mode-toggle
+    document.getElementById('mode-toggle').addEventListener('click', function() {
+        document.body.classList.toggle('light-mode');
+        const modeIcon = this.querySelector('i');
+        if (document.body.classList.contains('light-mode')) {
+            modeIcon.classList.remove('fa-moon');
+            modeIcon.classList.add('fa-sun');
+            localStorage.setItem('theme', 'light'); // Simpan status mode terang di localStorage
+        } else {
+            modeIcon.classList.remove('fa-sun');
+            modeIcon.classList.add('fa-moon');
+            localStorage.setItem('theme', 'dark'); // Simpan status mode gelap di localStorage
+        }
+    });
 });
+
 
 document.getElementById("bookmark-toggle").addEventListener("click", function() {
     if (this.innerText === "Bookmark") {
